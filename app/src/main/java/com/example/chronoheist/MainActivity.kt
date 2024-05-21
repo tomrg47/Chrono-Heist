d0d1fe9a1bb0029d22f6bd99a8c2bcfd92a0cf14
Hayden Pope <hayden.pope2004@gmail.com>
2024-05-21 18:19:58 +0100

    login window complete

--- app/src/main/java/com/example/chronoheist/MainActivity.kt
+++ app/src/main/java/com/example/chronoheist/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.chronoheist
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput=  findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i ("Test Credentials", "Username : $username and Password : $password ")
+
+        }
+
+
+
+
     }
 }
