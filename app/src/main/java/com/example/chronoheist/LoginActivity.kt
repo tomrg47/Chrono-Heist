625ee8124381353b4dde4baada0f2a816f89f02e
pointlessgamer <tomrg47@gmail.com>
2024-05-24 00:03:51 +0100

    Level select screen made with one functioning button - made a create account screen and made it so you can log in then click play game and be taken to the game instance

--- app/src/main/java/com/example/chronoheist/LoginActivity.kt
+++ app/src/main/java/com/example/chronoheist/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.chronoheist
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,10 +20,12 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var passwordInput : EditText
     private lateinit var loginBtn : Button
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,6 +35,9 @@ class LoginActivity : AppCompatActivity() {
         usernameInput = findViewById(R.id.username_input)
         passwordInput=  findViewById(R.id.password_input)
         loginBtn = findViewById(R.id.login_btn)
+        val login = findViewById<LinearLayout>(R.id.LogInLayout)
+        val signup = findViewById<LinearLayout>(R.id.SignUpLayout)
+        val signUpBtn = findViewById<Button>(R.id.createAccount_btn)
 
         loginBtn.setOnClickListener {
             val username = usernameInput.text.toString()
@@ -38,6 +47,10 @@ class LoginActivity : AppCompatActivity() {
             intent.putExtra("USERNAME", username)
             //intent.putExtra("PASSWORD", password)
             startActivity(intent)
+        }
+        signUpBtn.setOnClickListener{
+            login.visibility =View.GONE
+            signup.visibility= View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/example/chronoheist/MainActivity.kt
+++ app/src/main/java/com/example/chronoheist/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.chronoheist
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.TextView
+import android.widget.ImageButton
+import android.widget.TableLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +25,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val playBttn = findViewById<ImageButton>(R.id.playBttn)
+        val playFrame = findViewById<FrameLayout>(R.id.PlayBttnFrame)
+        val optionsFrame = findViewById<FrameLayout>(R.id.OptionsBttnFrame)
+        val armouryFrame = findViewById<FrameLayout>(R.id.ArmouryBttnFrame)
+        val battlepassFrame = findViewById<FrameLayout>(R.id.BattlePassBttnFrame)
+        val mapSelect = findViewById<TableLayout>(R.id.MapSelectTable)
+        val lvl1Bttn = findViewById<ImageButton>(R.id.level1Bttn)
+
+
+        playBttn.setOnClickListener {
+            playFrame.visibility = View.GONE
+            optionsFrame.visibility = View.GONE
+            armouryFrame.visibility = View.GONE
+            battlepassFrame.visibility = View.GONE
+            mapSelect.visibility = View.VISIBLE
+        }
+        lvl1Bttn.setOnClickListener{
+            val intent = Intent (this, Level1Activity::class.java)
+            startActivity(intent)
         }
     }
 }
